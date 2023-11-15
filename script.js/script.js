9526833e14f4bf037435c9ca561922dd28eccbd7
Lara Nishinakamasu <laranis02@gmail.com>
2023-11-15 18:33:24 -0300

    Javascript

---
+++ script.js/script.js
@@ -0,0 +1,25 @@
+
+
+let edad = parseFloat (prompt("Ingrese su edad"));
+if (edad >= 18) {
+    alert("Sos mayor de edad, podes pagar con tarjeta de crédito");
+} 
+else{ 
+    alert("Sos menor de edad, necesitas autorización de un adulto para realizar el pago de tu compra");
+} 
+
+
+function unaFuncion() {
+    console.log(1);
+    return "Buenas!";
+}
+const saludo = unaFuncion();
+console.log(saludo);
+
+
+
+let miElemento2 = document.getElementById("elemento");
+miElemento2.style.backgroundColor = "pink";
+
+
+
---
+++ script.js/script2.js
@@ -0,0 +1,16 @@
+
+
+
+
+let boton = document.getElementById("contactame");
+boton.addEventListener("click", function(){
+   let titulo = document.getElementById("enviar");
+   titulo.innerHTML = "<p>Se enviaron tus datos</p>";
+   titulo.style.color= "pink" 
+
+
+})
+
+
+
+
